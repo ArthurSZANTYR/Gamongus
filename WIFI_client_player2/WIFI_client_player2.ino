4d8abf0001e23286185c60c2ff34e678d76a51d7
ArthurSZANTYR <szantyrarthur.pto@gmail.com>
2023-11-22 15:29:58 +0100

    client ok recoit data du server

---
+++ WIFI_client_player2/WIFI_client_player2.ino
@@ -0,0 +1,87 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-client-server-wi-fi/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "ESP32-Access-Point-GAMONGUS";
+//const char* password = "123456789";
+
+//Your IP address or domain name with URL path
+const char* serverNameTemp = "http://192.168.4.1/temperature";
+//const char* serverNameHumi = "http://192.168.4.1/humidity";
+//const char* serverNamePres = "http://192.168.4.1/pressure";
+
+String temperature;
+String humidity;
+String pressure;
+
+unsigned long previousMillis = 0;
+const long interval = 5000; 
+
+void setup() {
+  Serial.begin(115200);
+  
+  WiFi.begin(ssid);   //, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) { 
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+  
+  if(currentMillis - previousMillis >= interval) {
+     // Check WiFi connection status
+    if(WiFi.status()== WL_CONNECTED ){ 
+      temperature = httpGETRequest(serverNameTemp);
+      Serial.println("Temperature: " + temperature);
+      
+      // save the last HTTP GET Request
+      previousMillis = currentMillis;
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+  }
+}
+
+String httpGETRequest(const char* serverName) {
+  WiFiClient client;
+  HTTPClient http;
+    
+  // Your Domain name with URL path or IP address with path
+  http.begin(client, serverName);
+  
+  // Send HTTP POST request
+  int httpResponseCode = http.GET();
+  
+  String payload = "--"; 
+  
+  if (httpResponseCode>0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    payload = http.getString();
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  // Free resources
+  http.end();
+
+  return payload;
+}
